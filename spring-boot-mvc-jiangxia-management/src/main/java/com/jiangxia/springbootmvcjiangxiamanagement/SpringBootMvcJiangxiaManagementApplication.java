4101cd342ee2143611e4c2d0f9f38a7484862f54
BinWayne <36578067@qq.com>
2018-03-03 22:06:42 +0800

    add project backend management

---
+++ spring-boot-mvc-jiangxia-management/src/main/java/com/jiangxia/springbootmvcjiangxiamanagement/SpringBootMvcJiangxiaManagementApplication.java
@@ -0,0 +1,12 @@
+package com.jiangxia.springbootmvcjiangxiamanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcJiangxiaManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvcJiangxiaManagementApplication.class, args);
+	}
+}
